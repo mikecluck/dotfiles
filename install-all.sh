e9305e8cc32e707a3cbd83c4663dfa210cc87a86
Mike Cluck <MCluck90@gmail.com>
2025-11-14 18:51:41 -0500

    Add an install script

---
+++ install-all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Make some folders that we're going to need
+mkdir -p ~/.local/bin ~/.fonts
+
+sudo apt-get install \
+  build-essential \
+  clang \
+  curl \
+  git \
+  vim
+
+# Kitty (https://sw.kovidgoyal.net/kitty/)
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install Starship
+curl -sS https://starship.rs/install.sh | sh
+
+# Install PNPM
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+echo "Next steps:"
+echo "1. Install JetBrainsMono Nerd Font (https://www.nerdfonts.com/font-downloads)"
+echo "  - Copy all ttf files to ~/.fonts/"
