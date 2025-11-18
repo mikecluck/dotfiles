09a039aca2b42e97828b26e2a546b7bab217b06d
Mike Cluck <MCluck90@gmail.com>
2025-11-17 22:45:50 -0500

    Setup VS Code exclusive neovim keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+--VS Code + vscode-nvim keybindings
+if vim.g.vscode then
+  vim.keymap.set(
+    "n",
+    "<leader>ge",
+    "<Cmd>lua require('vscode').action('editor.action.marker.next')<CR>",
+    { desc = "Go to next error" }
+  )
+  vim.keymap.set(
+    "n",
+    "<leader>gE",
+    "<Cmd>lua require('vscode').action('editor.action.marker.prev')<CR>",
+    { desc = "Go to previous error" }
+  )
+end
